448ea2959bd586b65014f8a53a4bab591bbcb9ec
Baruch Even <baruch@ev-en.org>
2014-08-13 19:08:11 +0300

    Move parsing of MPT structs to a different file
    In preparation for adding an offline parser for the debug files.

--- Makefile
+++ Makefile
@@ -2,8 +2,9 @@ VERSION=1.0
 CFLAGS=-O3 -g -Wall -Impt -DVERSION=\"${VERSION}\"
 
 all: mptevents
-mptevents: mptevents.o | Makefile
+mptevents: mptevents.o mptparser.o | Makefile
 mptevents.o: mptevents.c | Makefile
+mptparser.o: mptparser.c | Makefile
 tags: mptevents.c $(wildcard mpt/*.h) $(wildcard mpt/mpi/*.h)
 	ctags $^
 clean:
---
+++ mpt.h
@@ -0,0 +1,26 @@
+#ifndef MPTEVENTS_MPT_H
+#define MPTEVENTS_MPT_H
+
+#include <stdint.h>
+
+typedef uint8_t u8;
+typedef uint16_t __le16;
+typedef uint32_t __le32;
+typedef uint64_t __le64;
+
+#define __user
+
+#include "mpi/mpi2_type.h"
+#include "mpi/mpi2.h"
+#include "mpi/mpi2_cnfg.h"
+#include "mpi/mpi2_ioc.h"
+#include "mpt2sas_ctl.h"
+
+struct mpt_events {
+	struct mpt2_ioctl_header hdr;
+	struct MPT2_IOCTL_EVENTS event_data[MPT2SAS_CTL_EVENT_LOG_SIZE];
+};
+
+void dump_all_events(struct mpt_events *events, uint32_t *highest_context, int first_read);
+
+#endif
